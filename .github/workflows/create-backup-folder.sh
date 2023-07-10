718e230712dfa12e237ab65ec575b0ad0f0db876
Jonathan Soares <jonathan.soares91@outlook.com>
2023-07-10 09:54:51 -0300

    Delete create-backup-folder.sh

--- .github/workflows/create-backup-folder.sh
+++
@@ -1,7 +0,0 @@
- - name: Create backup folder
-        run: |
-          mkdir -p wordpress_data/wp-content/themes/twentyfifteen_tmp
-          mkdir -p wordpress_data/wp-content/themes/twentyfifteen_old
-          cp -R wordpress_data/wp-content/themes/twentyfifteen wordpress_data/wp-content/themes/twentyfifteen_tmp
-          rsync -avz --exclude=twentfifteen_old wordpress_data/wp-content/themes/twentyfifteen/ wordpress_data/wp-content/themes/twentfifteen_tmp/
-          cp -R wordpress_data/wp-content/themes/twentyfifteen_tmp wordpress_data/wp-content/themes/twentyfifteen
